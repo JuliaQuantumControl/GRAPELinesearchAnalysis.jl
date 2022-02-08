4b182ffed6a3c99f2d0a03c09269f703de9dd306
Michael Goerz <goerz@stanford.edu>
2022-02-08 17:52:47 -0500

    Use Plots.jl instead of PyPlot

--- src/GRAPELinesearchAnalysis.jl
+++ src/GRAPELinesearchAnalysis.jl
@@ -7,7 +7,8 @@ using GRAPE
 using Optim
 using LBFGSB
 using LinearAlgebra
-using PyPlot: matplotlib
+using Plots
+using Plots.PlotMeasures: px
 
 
 # Given a search direction (e.g., the negative gradient) and a vector of step
@@ -38,9 +39,9 @@ end
 
 
 function _get_linesearch_data(
-        wrk,
-        optimization_state::Optim.OptimizationState,
-        optimizer_state::Optim.AbstractOptimizerState,
+    wrk,
+    optimization_state::Optim.OptimizationState,
+    optimizer_state::Optim.AbstractOptimizerState,
 )
     # TODO: use wrk only (and compare with current implementation)
     α = optimizer_state.alpha # the stepwidth chosen by the linesearch
@@ -55,13 +56,13 @@ end
 
 
 function _get_linesearch_data(
-        wrk,
-        iter_res::GRAPE.LBFGSB_Result,
-        optimizer::LBFGSB.L_BFGS_B,
+    wrk,
+    iter_res::GRAPE.LBFGSB_Result,
+    optimizer::LBFGSB.L_BFGS_B,
 )
     # TODO: use wrk only (and compare with current implementation)
     lbfgs_direction = wrk.searchdirection
-    gradient_direction =  -1 * wrk.gradient
+    gradient_direction = -1 * wrk.gradient
     if norm(lbfgs_direction) ≈ 0.0
         # LBFGSB hasn't yet built up Hessian (first iteration)
         # TODO: this should be fixed in GRAPE
@@ -84,81 +85,133 @@ function _get_linesearch_data(
 end
 
 
-function plot_linesearch(outdir)
+function plot_linesearch(
+    outdir;
+    samples=100,
+    verbose=(get(ENV, "GRAPE_LINESEARCH_ANALYSIS_VERBOSE", "0") == "1"),
+    use_current_backend=(
+        get(ENV, "GRAPE_LINESEARCH_ANALYSIS_USE_CURRENT_BACKED", "0") == "1"
+    ),
+    kwargs...
+)
 
-    function _plot_linesearch(
-            wrk,
-            optimization_state,
-            optimizer_state,
-            iteration, args...)
+    defaults = Dict(
+        :linewidth => 3,
+        :marker_size => 3,
+        :foreground_color_legend => nothing,
+        :background_color_legend => RGBA(1, 1, 1, 0.8)
+    )
+    merge!(default, kwargs)
+
+    function _plot_linesearch(wrk, optimization_state, optimizer_state, iteration, args...)
+
+        current_backend = backend()
+        if !use_current_backend && !(current_backend isa Plots.GRBackend)
+            gr()
+        end
 
         (iteration == 0) && (return nothing)
 
         # TODO: linesearch data sources should be entirely in wrk, so we don't
         # need different methods.
-        α, lbfgs_direction, gradient_direction, pulsevals_guess, pulsevals_opt = _get_linesearch_data(wrk, optimization_state, optimizer_state)
-        α_vals = collect(range(0, 2α, length=100))
+        α, lbfgs_direction, gradient_direction, pulsevals_guess, pulsevals_opt =
+            _get_linesearch_data(wrk, optimization_state, optimizer_state)
+        α_vals = collect(range(0, 2α, length=samples))
         J_α_gradient = explore_linesearch(gradient_direction, α_vals, pulsevals_guess, wrk)
         J_α_lbfgs = explore_linesearch(lbfgs_direction, α_vals, pulsevals_guess, wrk)
         J_T = wrk.result.J_T
         J_T_guess = wrk.result.J_T_prev
 
-        fig, axs = matplotlib.pyplot.subplots(figsize = (7, 15), nrows=5)
-        fig.suptitle(
-            "Iteration $iteration; \$J_T\$ = $(@sprintf("%.2e", J_T_guess)) → $(@sprintf("%.2e", J_T))"
+        ax1 = plot(pulsevals_guess; label="guess", defaults...)
+        plot!(
+            ax1,
+            pulsevals_opt;
+            label="opt",
+            xlabel="time step (control parameter index)",
+            ylabel="control amplitude",
+            defaults...
+        )
+        plot!(
+            ax1,
+            title="Iteration $iteration; J = $(@sprintf("%.2e", J_T_guess)) → $(@sprintf("%.2e", J_T))"
         )
 
-        axs[1].plot(pulsevals_guess, label="guess")
-        axs[1].plot(pulsevals_opt, label="opt")
-        axs[1].set_xlabel("time step")
-        axs[1].set_ylabel("pulse amplitude")
-        axs[1].legend()
+        ax2 = plot(
+            gradient_direction;
+            label="gradient",
+            xlabel="time step (control parameter index)",
+            ylabel="gradient directon",
+            defaults...
+        )
 
-        axs[2].plot(gradient_direction, label="gradient")
-        axs[2].set_xlabel("time step")
-        axs[2].set_ylabel("gradient direction")
-        axs[2].legend()
+        ax3 = plot(gradient_direction; linestyle=:dash, label="gradient", defaults...)
+        plot!(
+            ax3,
+            lbfgs_direction;
+            label="LBFGS direction",
+            xlabel="time step (control parameter index)",
+            ylabel="LBFGS direction",
+            defaults...
+        )
 
-        axs[3].plot(gradient_direction, "--", label="gradient")
-        axs[3].plot(lbfgs_direction, label="LBFGS direction")
-        axs[3].set_xlabel("time step")
-        axs[3].set_ylabel("LBFGS direction")
-        axs[3].legend()
+        ax4 = plot(
+            α_vals,
+            J_α_gradient;
+            label="gradient",
+            xlabel="step width α",
+            ylabel="Functional",
+            title="Gradient Linesarch",
+            defaults...
+        )
+        scatter!(ax4, [0,], [J_T_guess,], label="")
+        hline!(ax4, [J_T_guess], color="black", linewidth=0.5, label="")
+        vline!(ax4, [α], color="black", linewidth=0.5, label="")
 
-        axs[4].plot(α_vals, J_α_gradient, label="gradient")
-        axs[4].scatter([0, ], [J_T_guess, ])
-        axs[4].axhline(y=J_T_guess, color="black", linewidth=0.5)
-        axs[4].axvline(x=α, color="black", linewidth=0.5)
-        axs[4].set_xlabel("step width α")
-        axs[4].set_ylabel("\$J_T\$")
-        axs[4].set_title("Gradient Linesearch")
-        axs[4].legend()
-
-        axs[5].plot(α_vals, J_α_gradient, "--", label="gradient")
-        axs[5].plot(α_vals, J_α_lbfgs, label="LBFGS")
-        axs[5].scatter([0, α], [J_T_guess, J_T])
-        axs[5].axhline(y=J_T_guess, color="black", linewidth=0.5)
-        axs[5].axhline(y=J_T, color="black", linewidth=0.5)
-        axs[5].axvline(x=α, color="black", linewidth=0.5)
-        axs[5].set_xlabel("step width α")
-        axs[5].set_ylabel("\$J_T\$")
+        ax5 = plot(
+            α_vals,
+            J_α_gradient;
+            linestyle=:dash,
+            label="gradient",
+            title="LBFGS Linesearch α = $α",
+            defaults...
+        )
+        plot!(
+            ax5,
+            α_vals,
+            J_α_lbfgs;
+            label="LBFGS",
+            xlabel="step width α",
+            ylabel="Functional",
+            defaults...
+        )
+        scatter!(ax5, [0, α], [J_T_guess, J_T], label="")
+        hline!(ax5, [J_T_guess], color="black", linewidth=0.5, label="")
+        hline!(ax5, [J_T], color="black", linewidth=0.5, label="")
+        vline!(ax5, [α], color="black", linewidth=0.5, label="")
         if J_T < 0.1
-            axs[5].set_yscale("log")
-            ylim = axs[5].get_ylim()
-            axs[5].set_ylim((ylim[1], 1))
+            plot!(ax5; yaxis=:log)
+            ylim = ylims(ax5)
+            ylims!(ax5, (ylim[1], 1))
         else
-            ylim = axs[5].get_ylim()
-            axs[5].set_ylim((ylim[1], 1))
+            ylim = ylims(ax5)
+            ylims!(ax5, (ylim[1], 1))
         end
-        axs[5].set_title("LBFGS Linesearch α = $α")
-        axs[5].legend()
 
-        fig.tight_layout()
+        fig = plot(ax1, ax2, ax3, ax4, ax5, layout=(5, 1),)
+        plot!(fig, size=(700, 1500), left_margin=50px,)
 
-        fig.savefig(
-            joinpath(outdir, @sprintf("linesearch-%03i.png", iteration))
-        )
+        outfile = joinpath(outdir, @sprintf("linesearch-%03i.png", iteration))
+        savefig(fig, outfile)
+        if verbose
+            @info "Written GRAPELinesearchAnalysis plot to $outfile"
+        end
+
+        if !use_current_backend && !(current_backend isa Plots.GRBackend)
+            current_backend() # switch back
+        end
+
         return nothing
+
     end
 
     return _plot_linesearch
