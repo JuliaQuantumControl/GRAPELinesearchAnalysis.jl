e74df010dea983b0339e48f742d2b444c464a164
Michael Goerz <goerz@stanford.edu>
2022-02-13 15:07:28 -0500

    Implement `make clean` in Julia

---
+++ clean.jl
@@ -0,0 +1,45 @@
+"""
+    clean([distclean=false])
+
+Clean up build/doc/testing artifacts. Restore to clean checkout state
+(distclean)
+"""
+function clean(; distclean=false, _exit=true)
+
+    _glob(folder, ending) =
+        [name for name in readdir(folder; join=true) if (name |> endswith(ending))]
+    _exists(name) = isfile(name) || isdir(name)
+    _push!(lst, name) = _exists(name) && push!(lst, name)
+
+    ROOT = @__DIR__
+
+    ###########################################################################
+    CLEAN = String[]
+    for folder in ["", "src"]
+        append!(CLEAN, _glob(joinpath(ROOT, folder), ".cov"))
+    end
+    ###########################################################################
+
+    ###########################################################################
+    DISTCLEAN = String[]
+    _push!(DISTCLEAN, joinpath(ROOT, "Manifest.toml"))
+    ###########################################################################
+
+    for name in CLEAN
+        @info "rm $name"
+        rm(name, force=true, recursive=true)
+    end
+    if distclean
+        for name in DISTCLEAN
+            @info "rm $name"
+            rm(name, force=true, recursive=true)
+        end
+        if _exit
+            @info "Exiting"
+            exit(0)
+        end
+    end
+
+end
+
+distclean() = clean(distclean=true)
